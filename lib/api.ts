605f1960fce1b6e64b26994d6b0801c209bf5b82
2mvs <119425975+2mvs@users.noreply.github.com>
2025-11-13 13:19:48 +0100

    Change API URL to localhost for developmentnew change

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://gestion-de-temps-3up6.vercel.app/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8008/api';
 
 const api = axios.create({
   baseURL: API_URL,
